9a82217bb2028added5774cc3b9ab388f602d883
ZazouSam <zazou2000@hotmail.ca>
2023-02-21 18:08:14 -0500

    modif

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,28 @@ Timeout t;
 //constant int for speed
 const int VITESSE_ANGULAIRE_MAX_DEG_SEC = 720;
 int target = 360;
-int ppr = 200; //point par tour 
+int pointParTour = 200; //point par tour 
 float vitesse = 0;
-float tpp; //time per point
+float tempsParPoints; //time per point
 
 float speed(int dps)
 {
   
   float period;
 
-  tpp = (dps/360.0) * ppr;
-  period = (1.0 / tpp)/2.0;
+  tempsParPoints = (dps/360.0) * pointParTour;
+  period = (1.0 / tempsParPoints)/2.0;
   return period;
 }
+
 void fct_step(void)
 {
   STEP = !STEP;
   // wait for target
   vitesse = speed(target);
   t.attach(&fct_step, vitesse);
-}	
+}
+
 int main()
 {
   t.attach(&fct_step, 1.0);
@@ -45,7 +47,7 @@ int main()
   {
     float adc = Pot.read_u16();
     float vitesse_angulaire = adc * VITESSE_ANGULAIRE_MAX_DEG_SEC / 65535.0;
-    float test = tpp *2 * 3.14159265359;
+    float test = tempsParPoints *2 * 3.14159265359;
     printf("vitesse = %f test = %fdegree/s\r\n", vitesse_angulaire, test);
     // convertir la vitesse angulaire en vitesse angulaire en deg/s
     target = vitesse_angulaire;
